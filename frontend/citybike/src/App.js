75b5c876d40366f6d3accc402fd32f4ae052580e
Ilmari Saari <ilmari.saari@tuni.fi>
2023-05-12 20:18:14 +0300

    Got tabs working for switching between journeys and stations. Dropdown sorting also somewhat functional, need some backend work to pass the sorting parameters

--- frontend/citybike/src/App.js
+++ frontend/citybike/src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
 import JourneyComponent from './components/JourneyComponent'
-
+import Tabs from './components/Tabs';
+import StationComponent from './components/StationComponent'
 function App() {
   return (
     <div className="App">
-      <JourneyComponent/>      
+          <Tabs>
+        <div label="Journeys">
+          <JourneyComponent></JourneyComponent>
+        </div>
+        <div label="Stations">
+          <StationComponent></StationComponent>
+        </div>
+      </Tabs>
+            
     </div>
   );
 }
---
+++ frontend/citybike/src/components/StationComponent.js
@@ -0,0 +1,180 @@
+import axios from 'axios';
+import React from 'react';
+
+class JourneyComponent extends React.Component {
+
+
+    constructor(props) {
+
+        super(props)
+
+        this.state = {
+
+            stations: [],
+
+            currentPage: 1,
+
+            recordsPerPage: 30,
+            
+            totalPages: 0,
+
+            sortBy: "id"
+
+        }
+    }
+
+    componentDidMount() {
+        this.fetchStationsByPage(this.state.currentPage);
+        this.fetchTotalPages(this.state.recordsPerPage); 
+    }
+    	
+    
+
+    fetchTotalPages(recordsPerPage){
+        axios.get("http://localhost:8080/stations/count")
+        .then(response => response.data).then((data) => {
+            this.setState({
+                totalPages: Math.ceil(data / recordsPerPage)
+
+            })
+        })
+    }
+
+    showLastPage = () => {
+        
+        if(this.state.currentPage >= this.state.totalPages){
+            return;
+        }
+
+        this.setState({
+            currentPage: this.state.totalPages
+        }) 
+
+        this.fetchStationsByPage(this.state.totalPages)
+
+    }
+
+    showFirstPage = () => {
+        
+        let firstPage = 1;
+
+        if(this.state.currentPage < firstPage){
+            return
+        }
+            
+        this.setState({
+            currentPage: firstPage
+        })
+
+        this.fetchStationsByPage(firstPage)
+
+    }
+
+    showNextPage = () => {
+        
+        if(this.state.currentPage >= this.state.totalPages){
+            return;
+        }
+
+        const newPage = this.state.currentPage + 1;
+
+        this.setState({
+            currentPage: newPage
+        })
+
+        this.fetchStationsByPage(newPage);
+
+    }
+
+
+    showPreviousPage = () => {
+        
+
+
+        if(this.state.currentPage <= 1){
+            return;
+        }
+
+        const newPage = this.state.currentPage - 1;
+
+        this.setState({
+            currentPage: newPage
+        })
+
+        this.fetchStationsByPage(newPage);
+
+    }
+
+    fetchStationsByPage(pageNumber) {
+
+
+        pageNumber -= 1
+
+        axios.get("http://localhost:8080/stations?pageNumber=" + pageNumber
+            + "&pageSize=" + this.state.recordsPerPage)
+            .then(response => response.data).then((data) => {
+                this.setState({
+                    stations: data
+
+                })
+
+            })
+    }
+
+    render() {
+        const { stations, currentPage, recordsPerPage, totalPages } = this.state;
+
+        return (
+            <div>
+
+                <h1 className="text-center mt-5 ">All bike journeys</h1>
+                <div className="container mt-2">
+                    <table className="table table-bordered border-info shadow">
+                        <thead>
+                            <tr>
+                                <th>Index</th>
+                                <th>Station name</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {stations.length === 0 ?
+                                <tr align="center"><td colSpan="5">No stations found</td></tr> :
+                                stations.map(
+                                    (station, index) => (
+
+                                        <tr key={station.id}>
+                                            <td>{(recordsPerPage * (currentPage - 1)) + index + 1}</td>
+                                            <td>{station.stationName}</td>
+                                        </tr>
+                                    )
+                                )
+                            }
+                        </tbody>
+                    </table>
+                    <table className="table">
+                        <div style={{ float: 'left', fontFamily: 'monospace', color: '#0275d8' }}>
+                            Page {currentPage} of {totalPages}
+                        </div>
+                        <div style={{ float: 'right' }}>
+                            <div class="clearfix"></div>
+                            <nav aria-label="Page navigation example">
+                                <ul class="pagination">
+                                    <li class="page-item"><a type="button" class="page-link" disabled={currentPage === 1 ? true : false} onClick={this.showPreviousPage}>Previous</a></li>
+                                    <li class="page-item"><a type="button" class="page-link" disabled={currentPage === 1 ? true : false} onClick={this.showFirstPage}>First</a></li>
+                                    <li class="page-item"><a type="button" class="page-link" disabled={currentPage === totalPages ? true : false} onClick={this.showNextPage}>Next</a></li>
+                                    <li class="page-item"><a type="button" class="page-link" disabled={currentPage === totalPages ? true : false} onClick={this.showLastPage}>Last</a></li>
+                                </ul>
+                            </nav>
+                        </div>
+                        
+
+
+
+                    </table>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default JourneyComponent;
